26c187c0c8f83e3163c59c44fa8614d3c9ae1a6e
Sebastia1111 <seba.orellanav@duocuc.cl>
2026-05-13 14:53:07 -0400

    Add comment to repo interface for clarity
    Added comment to indicate the purpose of the repository.

--- Store/catalogo/catalogo/src/main/java/com/tienda/catalogo/Repo/repo.java
+++ Store/catalogo/catalogo/src/main/java/com/tienda/catalogo/Repo/repo.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Repository;
 import com.tienda.catalogo.Productos.Ropa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+// este es el repositorio
 @Repository
 public interface repo extends JpaRepository<Ropa, Long> {
     
