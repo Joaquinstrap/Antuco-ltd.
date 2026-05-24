35cae05a3eab2101302e29594a0e5efa7826c4f3
nicchavez-duocuc <nic.chavez@duocuc.cl>
2026-05-24 02:22:16 -0400

    Update V1__Crear_Tabla_Usuarios.sql

--- Servicio Usuarios/usuarios/src/main/resources/db/migration/V1__Crear_Tabla_Usuarios.sql
+++ Servicio Usuarios/usuarios/src/main/resources/db/migration/V1__Crear_Tabla_Usuarios.sql
@@ -1,5 +1,6 @@
 CREATE TABLE usuarios (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    rol VARCHAR(255)
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username    VARCHAR(255) NOT NULL UNIQUE,
+    clave       VARCHAR(255) NOT NULL,
+    rol         VARCHAR(255) -- Permisos del usuario. "User", "Admin", etc.
 );
