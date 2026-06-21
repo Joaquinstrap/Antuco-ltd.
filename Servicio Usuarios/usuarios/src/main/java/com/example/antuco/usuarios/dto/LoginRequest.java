f948ec602cd63cae5dfe7568eed2b5e51aa8bd4f
Sebastia1111 <seba.orellanav@duocuc.cl>
2026-06-21 18:07:39 -0400

    Delete Servicio Usuarios/usuarios directory

--- Servicio Usuarios/usuarios/src/main/java/com/example/antuco/usuarios/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.antuco.usuarios.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String username;
-    private String clave;
-}
--- Servicio Usuarios/usuarios/target/classes/db/migration/V1__Crear_Tabla_Usuarios.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE usuarios (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    rol VARCHAR(255)
-);
