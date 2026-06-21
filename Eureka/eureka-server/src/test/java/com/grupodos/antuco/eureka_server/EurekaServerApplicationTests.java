ff7baa945eb8a20307c27a0b3f5fffa047d7256c
Sebastia1111 <seba.orellanav@duocuc.cl>
2026-06-20 22:19:24 -0400

    Add files via upload

---
+++ Eureka/eureka-server/src/test/java/com/grupodos/antuco/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupodos.antuco.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
