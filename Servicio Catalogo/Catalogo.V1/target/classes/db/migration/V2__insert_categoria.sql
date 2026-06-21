0061e86c702ad6113dc1a5e1e5bba5d6b9c42a30
Sebastia1111 <seba.orellanav@duocuc.cl>
2026-06-21 14:45:37 -0400

    Catalogo Java21 + Eureka

---
+++ Servicio Catalogo/Catalogo.V1/target/classes/db/migration/V2__insert_categoria.sql
@@ -0,0 +1,3 @@
+-- Insertar categorías principales con ID en formato VARCHAR
+INSERT INTO categorias (id, nombre_categoria) VALUES ('01', 'Música_Física');
+INSERT INTO categorias (id, nombre_categoria) VALUES ('02', 'Ropa');
