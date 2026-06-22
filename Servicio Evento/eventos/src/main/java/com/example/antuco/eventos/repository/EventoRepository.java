1a1783cb2d833ef72c04d15278670b5109dad8ac
Sebastia1111 <seba.orellanav@duocuc.cl>
2026-06-22 12:27:34 -0400

    Delete Servicio Evento/eventos directory

--- Servicio Evento/eventos/src/main/java/com/example/antuco/eventos/repository/EventoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.antuco.eventos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.antuco.eventos.model.Evento;
-
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-}
