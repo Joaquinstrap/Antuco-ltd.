e8547d227e7ce85ad0511608cd638f6504d8c0a1
Sebastia1111 <seba.orellanav@duocuc.cl>
2026-06-22 16:48:16 -0400

    Delete Servicio Inventario/inventario/inventario directory

--- Servicio Inventario/inventario/inventario/target/classes/db/migration/V1__Creacio_invTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE inventarios (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    producto_Id BIGINT NOT NULL UNIQUE,
-    cantidad INT NOT NULL DEFAULT 0,
-    tipo_producto VARCHAR(255) NOT NULL,
-
-    /* Esta linea sirve como una validacion que dice que no puede haber una cantidad menor a 0 */
-    CONSTRAINT check_cantidad_no_negativa CHECK (cantidad >= 0)
-);
