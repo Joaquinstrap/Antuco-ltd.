fb0be6beaca0802a92fe01c76db3e9a42f3c0d23
Sebastia1111 <seba.orellanav@duocuc.cl>
2026-06-21 20:41:27 -0400

    Delete Servicio Pago/pago directory

--- Servicio Pago/pago/src/main/java/com/antuco/pago/repository/PagoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.antuco.pago.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.antuco.pago.model.Pago;
-
-public interface PagoRepository extends JpaRepository<Pago, Long> {
-}
