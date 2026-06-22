521e837b517ac28cbe1290f430e673efe279bff6
Sebastia1111 <seba.orellanav@duocuc.cl>
2026-06-21 20:23:13 -0400

    Delete Servicio Autenticacion/autenticacion directory

--- Servicio Autenticacion/autenticacion/src/main/java/com/example/antuco/autenticacion/repository/CredencialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.antuco.autenticacion.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.antuco.autenticacion.model.Credencial;
-
-public interface CredencialRepository extends JpaRepository<Credencial, Long> {
-    Credencial findByUsername(String username);
-}
