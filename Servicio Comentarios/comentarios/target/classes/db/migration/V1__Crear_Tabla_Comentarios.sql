291b8386d64e4d464ec6d91d58157931932e3612
Sebastia1111 <seba.orellanav@duocuc.cl>
2026-06-21 21:01:13 -0400

    Delete Servicio Comentarios/comentarios directory

--- Servicio Comentarios/comentarios/target/classes/db/migration/V1__Crear_Tabla_Comentarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE comentarios (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    producto_id BIGINT NOT NULL, -- ID del producto en el servicio Catálogo
-    usuario_username VARCHAR(255) NOT NULL, -- Username del usuario (del servicio Usuarios)
-    texto TEXT NOT NULL,
-    calificacion INT NOT NULL, -- Estrellas (1 a 5)
-    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
