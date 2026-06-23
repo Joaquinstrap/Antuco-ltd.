7290641bc9fcd4dba8fcc1c1cadcd0d4762f02b7
Sebastia1111 <seba.orellanav@duocuc.cl>
2026-06-23 10:27:10 -0400

    Delete Servicio Pedidos/pedidos directory

--- Servicio Pedidos/pedidos/src/main/java/com/example/antuco/pedidos/repository/PedidoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.antuco.pedidos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.antuco.pedidos.model.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
