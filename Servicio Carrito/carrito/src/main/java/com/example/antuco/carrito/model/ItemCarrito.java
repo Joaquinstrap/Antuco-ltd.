a22456a6a48c6b25accf76453e49332412a28931
nicchavez-duocuc <nic.chavez@duocuc.cl>
2026-06-23 22:35:27 -0400

    Documentacion Swagger

--- Servicio Carrito/carrito/src/main/java/com/example/antuco/carrito/model/ItemCarrito.java
+++ Servicio Carrito/carrito/src/main/java/com/example/antuco/carrito/model/ItemCarrito.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -23,6 +24,7 @@ public class ItemCarrito {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID de la transaccion del producto al carrito", example = "001")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -31,10 +33,18 @@ public class ItemCarrito {
     private Carrito carrito;
 
     // Datos que vienen del servicio de catálogo (Snapshot)
+    @Schema(description = "ID original del producto", example = "001")
     private Long productoId; 
+
+    @Schema(description = "Nombre del producto", example = "Polera de Banda")
     private String nombreProducto;
+
+    @Schema(description = "Precio unitario del producto", example = "$19.990")
     private BigDecimal precioUnitario;
+
+    @Schema(description = "URL de la imagen del producto", example = "https://www.tienda.com/assets/album1/cover.jpg")
     private String imagenUrl;
 
+    @Schema(description = "Cantidad de unidades pedidas del producto", example = "2")
     private Integer cantidad;
 }
