8cd78be726f3b4d9da8d3cec01eb2896a5b7d13d
nicchavez-duocuc <nic.chavez@duocuc.cl>
2026-06-23 22:30:27 -0400

    Documentacion Swagger

--- Servicio Usuarios/usuarios/src/main/java/com/example/antuco/usuarios/model/Usuario.java
+++ Servicio Usuarios/usuarios/src/main/java/com/example/antuco/usuarios/model/Usuario.java
@@ -1,5 +1,6 @@
 package com.example.antuco.usuarios.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -19,13 +20,17 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "El ID del usuario", example = "001")
     private Long id;
 
     @Column(unique = true, nullable = false)
+    @Schema(description = "el nombre de usuario", example = "vegetta777")
     private String username; // Usaremos esto como el ID para el carrito (ej: email)
 
     @Column(nullable = false)
+    @Schema(description = "La clave del usuario", example = "Servicios.200")
     private String clave;
     
+    @Schema(description = "El rol del usuario", example = "ADMIN")
     private String rol; // Ej: "USER", "ADMIN"
 }
